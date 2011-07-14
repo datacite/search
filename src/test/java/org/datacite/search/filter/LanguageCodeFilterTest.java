265132f57aefc98dd1953288e243691c17057235
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-07-14 14:10:15 +0200

    add test for language filter

---
+++ src/test/java/org/datacite/search/filter/LanguageCodeFilterTest.java
@@ -0,0 +1,56 @@
+package org.datacite.search.filter;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.KeywordTokenizer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.solr.analysis.BaseTokenTestCase;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class LanguageCodeFilterTest extends BaseTokenTestCase {
+
+    LanguageCodeFilterFactory filterFactory = new LanguageCodeFilterFactory();
+
+    @Test
+    public void testTwoLetter() {
+        assertFilter("en", "en");
+    }
+
+    @Test
+    public void testThreeLetterToTwoLetter() {
+        assertFilter("deu", "de");
+        assertFilter("eng", "en");
+        assertFilter("zho", "zh");
+    }
+
+    @Test
+    public void testThreeLetterOnly() {
+        assertFilter("tlh", "tlh"); // Klingon
+    }
+    
+    @Test
+    public void testCountryCode() {
+        assertFilter("en_GB", "en");
+    }
+    
+    @Ignore
+    @Test
+    public void testThreeLetterAlternatives() {
+        assertFilter("ger", "de");
+        assertFilter("chi", "zh");
+    }
+
+    private void assertFilter(String in, String... out) {
+        Reader reader = new StringReader(in);
+        TokenStream token = new KeywordTokenizer(reader);
+        TokenStream filtered = filterFactory.create(token);
+        try {
+            assertTokenStreamContents(filtered, out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
