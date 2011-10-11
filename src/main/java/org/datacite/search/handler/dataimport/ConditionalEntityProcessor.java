c901203837177b94a1ab256ba9f8c8ae79004765
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-10-11 15:33:12 +0200

    better handling of boolean for data import handler

--- src/main/java/org/datacite/search/handler/dataimport/ConditionalEntityProcessor.java
+++ src/main/java/org/datacite/search/handler/dataimport/ConditionalEntityProcessor.java
@@ -3,6 +3,7 @@ package org.datacite.search.handler.dataimport;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.BooleanUtils;
 import org.apache.solr.handler.dataimport.Context;
 import org.apache.solr.handler.dataimport.EntityProcessorBase;
 
@@ -17,7 +18,21 @@ public class ConditionalEntityProcessor extends EntityProcessorBase {
         super.init(context);
         String conditionalField = context.getEntityAttribute(CONDITIONAL_ATTRIBUTE);
         Object condition = context.resolve(conditionalField);
-        skip = condition == null;
+        skip = ! toBoolean(condition);
+    }
+    
+    private boolean toBoolean(Object obj) {
+        if (obj == null)
+            return false;
+        else {
+            String str = obj.toString();
+            if (str.equals("1"))
+                return true;
+            else if (str.equals("0"))
+                return false;
+            else
+                return BooleanUtils.toBoolean(str);
+        }
     }
 
     @Override
