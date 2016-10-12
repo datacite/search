5893300a4a9af488a49f3f4088ae2e3b98e7cb30
Kristian Garza <kj.garza@gmail.com>
2016-10-12 11:48:55 +0200

    add context in dockers

--- docker/70_templates.sh
+++ docker/70_templates.sh
@@ -3,5 +3,6 @@ dockerize -template /home/app/docker/solrconfig.xml.tmpl:/home/app/src/main/reso
 dockerize -template /home/app/docker/solrcore.properties.tmpl:/home/app/src/main/resources/solrcore.properties
 dockerize -template /home/app/docker/log4j.xml.tmpl:/home/app/src/main/resources/log4j.xml
 dockerize -template /home/app/docker/crontab.tmpl:/home/app/docker/crontab
+dockerize -template /home/app/docker/context.xml.tmpl:/home/app/src/main/webapp/META-INF/context.xml
 
 crontab /home/app/docker/crontab
