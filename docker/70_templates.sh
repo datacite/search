83dde373c8e33b2ea71143f13b25e47bfd4a2bac
Martin Fenner <mfenner@datacite.org>
2017-12-30 19:10:32 +0100

    clean up templates

--- docker/70_templates.sh
+++ docker/70_templates.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-# dockerize -template /home/app/docker/solrconfig.xml.tmpl:/home/app/src/main/resources/solrconfig.xml
-# dockerize -template /home/app/docker/data-config.xml.tmpl:/home/app/src/main/resources/data-config.xml
-# dockerize -template /home/app/docker/solrcore.properties.tmpl:/home/app/src/main/resources/solrcore.properties
-# dockerize -template /home/app/docker/log4j.xml.tmpl:/home/app/src/main/resources/log4j.xml
-# dockerize -template /home/app/docker/crontab.tmpl:/home/app/docker/crontab
 
 dockerize -template /home/app/docker/context.xml.tmpl:/home/app/src/main/webapp/META-INF/context.xml
 dockerize -template /home/app/docker/log4j.properties.tmpl:/home/app/src/main/resources/log4j.properties
@@ -23,5 +18,3 @@ cp /home/app/solr_home/collection1/conf/*.html /data/solr/collection1/conf/
 chown tomcat7. /data/solr/* -R
 chmod a+w /data/solr/* -R
 chmod a+xr /usr/local/bin/db_check
-
-# crontab /home/app/docker/crontab
